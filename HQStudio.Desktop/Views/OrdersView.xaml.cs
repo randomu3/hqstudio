51f177660097301dfaf7b3a019e02dd24e0538a9
randomu3 <demiz99@mail.ru>
2025-12-23 22:21:26 +0500

    fix(desktop): восстановлен оригинальный UI заказов с CardHoverable стилем

--- HQStudio.Desktop/Views/OrdersView.xaml.cs
+++ HQStudio.Desktop/Views/OrdersView.xaml.cs
@@ -3,14 +3,11 @@ using HQStudio.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace HQStudio.Views
 {
     public partial class OrdersView : UserControl
     {
-        private Border? _selectedBorder;
-        
         public OrdersView()
         {
             InitializeComponent();
@@ -18,20 +15,12 @@ namespace HQStudio.Views
 
         private void Order_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (sender is Border border && border.DataContext is Order order)
+            if (sender is FrameworkElement element && element.DataContext is Order order)
             {
                 if (DataContext is OrdersViewModel vm)
                 {
-                    // Сбрасываем предыдущее выделение
-                    if (_selectedBorder != null)
-                    {
-                        _selectedBorder.BorderBrush = Brushes.Transparent;
-                    }
-                    
-                    // Выделяем текущий заказ
+                    // Одиночный клик - выбор заказа
                     vm.SelectedOrder = order;
-                    border.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4CAF50"));
-                    _selectedBorder = border;
                     
                     // Двойной клик - редактирование
                     if (e.ClickCount == 2)
